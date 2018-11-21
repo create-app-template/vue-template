0daddd6e91771661d9118dfb413fdab1783079db
yunlong.wang@okcoin.com <yunlong.wang@okcoin.com>
2018-11-21 21:19:32 +0800

    修改模板

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <template>
   <div>
-    <Header/>
     <router-link to="/">首页</router-link>
     <router-link to="/bar">bar</router-link>
     <router-link to="/foo">foo</router-link>
     <router-view></router-view>
-    <Footer/>
   </div>
 </template>
 
 <script>
-import Header from './components/Header';
-import Footer from './components/Footer';
 export default {
   name: 'App',
-  components: {
-    Header,
-    Footer
-  }
 }
 </script>
 
